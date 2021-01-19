fa020167e8923c3f5c2d72b95387431c1975ca6b
Humphrey Swanzy <sway@humphreyswanzy.com>
2021-01-19 23:44:20 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// var showcase = document.querySelector('.showcase')
+
+// var square1 = document.getElementById('square1')
+// square1.addEventListener("click", function(){
+    
+//     showcase.style.backgroundImage = ''
+//     showcase.style.backgroundColor = 'purple'
+// })
+
+// var square2 = document.getElementById('square2')
+// square2.addEventListener("click", function(){
+//     showcase.style.backgroundImage = "url('https://static.dezeen.com/uploads/2018/07/the-pearl-skyscraper-movie-dwayne-johnson_dezeen_hero.jpg')";
+// })
+
+// var square3 = document.getElementById('square3')
+// square3.addEventListener("mouseover", function(){
+//     showcase.style.backgroundColor = 'red'
+// })
